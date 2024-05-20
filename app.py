19e485cb8874baff5a353c3001919ce8eeb6517d
xiaoquan006zhao <xiaoquan0622@gmail.com>
2024-05-20 11:11:49 -0500

    adding comments and clean up code structure

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 import json
 from sys import platform
 from PIL import Image, ImageTk
-from camera_setup import create_devices_with_tries, Camera_On, height1, width1
+from camera_setup import create_devices_with_tries, Camera_On, Camera_off
 
 # Load calibration data
 mapx = mapy = None
@@ -22,13 +22,18 @@ newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
 x, y, w, h = roi
 
+# MAC addresses of the cameras to be used in order
 MAC_list = ["1C:0F:AF:0D:05:91", "1C:0F:AF:3D:3F:15", "1C:0F:AF:03:6B:4E", "1C:0F:AF:0E:B3:2D"]
 # MAC_list = ["1C:0F:AF:3D:3F:15", "1C:0F:AF:03:6B:4E"]
 
+
+# Function to thread safe print to the console
 def safe_print(*args, **kwargs):
     with threading.Lock():
         print(*args, **kwargs)
 
+
+# Function to convert an integer to a MAC address string
 def int_to_mac(mac_value):
     try:
         mac_bytes = [
@@ -43,6 +48,7 @@ def int_to_mac(mac_value):
         return mac_address
     except Exception as e:
         return f"Error converting MAC address: {e}"
+
 
 class ImageSaverApp:
     def __init__(self, root, save_directory_path, Set_exposure):
@@ -59,63 +65,65 @@ class ImageSaverApp:
         self.subfolder_name = tk.StringVar()
 
         # Main area for image
-        self.main_frame = tk.Frame(root, height=300, width=500, bg='lightgreen')
+        self.main_frame = tk.Frame(root, height=300, width=500, bg="lightgreen")
         self.main_frame.grid(row=0, column=0, rowspan=6, padx=20, pady=20, sticky="nsew")
         self.main_frame.grid_propagate(False)
 
         # Initial image placeholder
         self.img_label = tk.Label(self.main_frame)
-        self.img_label.pack(fill='both', expand=True)
+        self.img_label.pack(fill="both", expand=True)
 
         # Buttons and inputs
         self.prefix_label = tk.Label(root, text="Naming Prefix:")
-        self.prefix_label.grid(row=0, column=1, padx=10, pady=10, sticky='ew')
+        self.prefix_label.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
 
         self.prefix_entry = tk.Entry(root, textvariable=self.naming_prefix, width=20)
-        self.prefix_entry.grid(row=0, column=2, padx=10, pady=10, sticky='ew')
+        self.prefix_entry.grid(row=0, column=2, padx=10, pady=10, sticky="ew")
 
         self.count_label = tk.Label(root, text="Image Counter:")
-        self.count_label.grid(row=1, column=1, padx=10, pady=10, sticky='ew')
+        self.count_label.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
 
         self.count_entry = tk.Entry(root, textvariable=self.image_count, width=20)
-        self.count_entry.grid(row=1, column=2, padx=10, pady=10, sticky='ew')
+        self.count_entry.grid(row=1, column=2, padx=10, pady=10, sticky="ew")
 
         self.subfolder_label = tk.Label(root, text="Subfolder Name:")
-        self.subfolder_label.grid(row=2, column=1, padx=10, pady=10, sticky='ew')
+        self.subfolder_label.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
 
         self.subfolder_entry = tk.Entry(root, textvariable=self.subfolder_name, width=20)
-        self.subfolder_entry.grid(row=2, column=2, padx=10, pady=10, sticky='ew')
+        self.subfolder_entry.grid(row=2, column=2, padx=10, pady=10, sticky="ew")
 
         self.image_count_label = tk.Label(root, text=f"Images Saved: {self.count - 1}")
-        self.image_count_label.grid(row=3, column=1, padx=10, pady=10, sticky='ew')
+        self.image_count_label.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
 
         self.button1 = tk.Button(root, text="Open Explorer", command=self.open_explorer, height=2, width=20)
-        self.button1.grid(row=4, column=1, padx=10, pady=10, sticky='ew')
+        self.button1.grid(row=4, column=1, padx=10, pady=10, sticky="ew")
 
         self.button2 = tk.Button(root, text="Save", command=self.save_image, height=2, width=20)
-        self.button2.grid(row=5, column=1, padx=10, pady=10, sticky='ew')
+        self.button2.grid(row=5, column=1, padx=10, pady=10, sticky="ew")
 
         self.button3 = tk.Button(root, text="Start", command=self.start_process, height=2, width=20)
-        self.button3.grid(row=6, column=1, padx=10, pady=10, sticky='ew')
+        self.button3.grid(row=6, column=1, padx=10, pady=10, sticky="ew")
 
     def camera_init(self):
+        # Initialize the cameras, their thread events, and main app thread condition
         self.frame_list = [None] * len(MAC_list)
         self.threading_event_list = [None] * len(MAC_list)
         self.threading_condition = threading.Condition()
 
         devices = create_devices_with_tries()
 
+        # Initialize the cameras in MAC_list order
         for i in range(len(devices)):
             event = threading.Event()
             mac_address = int_to_mac(devices[i].nodemap.get_node("GevMACAddress").value)
+            assert mac_address in MAC_list, f"MAC address {mac_address} not found in MAC_list"
+            index = MAC_list.index(mac_address)
 
-            if mac_address in MAC_list:
-                index = MAC_list.index(mac_address)
-                self.threading_event_list[index] = event
-                self.frame_list[index] = Camera_On(self.Set_exposure, index, devices[i], event, self.threading_condition)
-            else:
-                safe_print(f"MAC address {mac_address} not found in MAC_list")
+            # Populate frame_list and threading_event_list
+            self.threading_event_list[index] = event
+            self.frame_list[index] = Camera_On(self.Set_exposure, index, devices[i], event, self.threading_condition)
 
+        # Wait for all cameras to negotiate PTP Sync
         i = 0
         while True:
             masterFound = False
@@ -123,12 +131,12 @@ class ImageSaverApp:
 
             for frame in self.frame_list:
                 device = frame.device
-                ptpStatus = device.nodemap.get_node('PtpStatus').value
+                ptpStatus = device.nodemap.get_node("PtpStatus").value
                 camera_index = int(frame.which_camera)
 
-                device.nodemap.get_node('GevSCPD').value = 240000
+                device.nodemap.get_node("GevSCPD").value = 240000
                 offsetTime = camera_index * 80000
-                device.nodemap.get_node('GevSCFTD').value = offsetTime
+                device.nodemap.get_node("GevSCFTD").value = offsetTime
 
                 if ptpStatus == "Master":
                     if masterFound:
@@ -143,38 +151,45 @@ class ImageSaverApp:
             if not restartSyncCheck and masterFound:
                 break
 
-            time.sleep(0.5)
+            time.sleep(1)
             i += 1
             print(f"Trying {i}th Negotiate. MasterFound: {masterFound}.")
 
-        for frame in self.frame_list:
-            device = frame.device
-            device.nodemap.get_node('AcquisitionStartMode').value = 'PTPSync'
-
-    def start_process(self):
+    # Function to handle button click event style and return the original text and color
+    def button_click(self, button, display_text, bg_color="red"):
         original_text = self.button3.cget("text")
         original_color = self.button3.cget("bg")
-        self.button3.config(state="disabled", text="Starting...", bg="red")
+        button.config(state="disabled", text=display_text, bg=bg_color)
         self.root.update_idletasks()
 
+        return original_text, original_color
+
+    # Function to revert button style to original text and color
+    def revert_button(self, button, original_text, original_color):
+        button.config(state="normal", text=original_text, bg=original_color)
+        self.root.update_idletasks()
+
+    def start_process(self):
+        # Change button style to show that the process has started
+        original_text, original_color = self.button_click(self.button3, display_text="Starting...")
+
+        # For each frame, start the process in a separate thread
         for frame in self.frame_list:
             frame.startProcess()
 
-        def revert_button():
-            self.button3.config(state="normal", text=original_text, bg=original_color)
-            self.root.update_idletasks()
-
-        self.root.after(2000, revert_button)
+        # Pass the revert_button function and its arguments
+        self.root.after(2000, lambda: self.revert_button(self.button3, original_text, original_color))
         threading.Thread(target=self.view_save_loop, daemon=True).start()
 
     def view_save_loop(self):
+        # Continuously check if all frames are ready to be displayed
         while True:
             with threading.Lock():
+                # Wait for all frames to be ready
                 for threading_event in self.threading_event_list:
                     threading_event.wait()
 
                 buffer_list = []
-
                 for index, frame in enumerate(self.frame_list):
                     img_array = frame.read()
                     buffer_list.append(img_array)
@@ -182,9 +197,11 @@ class ImageSaverApp:
                 safe_print(f"All buffer Ready! {len(buffer_list)} frames received")
                 self.view_image(buffer_list)
 
+                # Clear all threading events for next iteration
                 for threading_event in self.threading_event_list:
                     threading_event.clear()
 
+                # Notify all frames to start capturing the next image
                 with self.threading_condition:
                     self.threading_condition.notify_all()
 
@@ -194,69 +211,73 @@ class ImageSaverApp:
         height = h
         width = w
 
+        # Create a 2x2 grid to display the images
         combined_images = np.zeros((2 * height, 2 * width, buffer_bytes_per_pixel), dtype=np.uint8)
-        images = []
 
         for _, (npndarray, i) in enumerate(image_array_list):
+            # Preprocess: lighting adjustment, undistortion, and cropping
             npndarray = cv2.convertScaleAbs(npndarray, alpha=10, beta=60)
             dst = cv2.remap(npndarray, mapx, mapy, cv2.INTER_LINEAR)
-            dst = dst[y:y+h, x:x+w]
-            images.insert(i, dst)
+            dst = dst[y : y + h, x : x + w]
 
-            row = i // 2
-            col = i % 2
-            combined_images[row * height:(row + 1) * height, col * width:(col + 1) * width, :] = dst
+            # put the image in the right place in the 2x2 grid
+            row, col = divmod(i, 2)
+            combined_images[row * height : (row + 1) * height, col * width : (col + 1) * width, :] = dst
 
+        # Resize the combined image and display it
         view_image = cv2.resize(combined_images, (0, 0), fx=0.2, fy=0.2)
+        # Save the combined image to the image_buffer for future saving
+        self.image_buffer = combined_images
+
+        # Convert to PIL image and then to PhotoImage
         photo_img = Image.fromarray(cv2.cvtColor(view_image, cv2.COLOR_BGR2RGB))
         photo_img = ImageTk.PhotoImage(photo_img)
         self.img_label.config(image=photo_img)
         self.img_label.image = photo_img
 
-        self.image_buffer = combined_images
-
     def save_image(self):
-        if self.image_buffer is not None:
-            original_text = self.button2.cget("text")
-            original_color = self.button2.cget("bg")
-            self.button2.config(state="disabled", text="Saving...", bg="red")
-            self.root.update_idletasks()
+        assert self.image_buffer is not None, "No image to save"
+        original_text, original_color = self.button_click(self.button2, display_text="Saving...")
 
-            prefix = self.naming_prefix.get().strip()
-            subfolder = self.subfolder_name.get().strip()
-            count = self.image_count.get().strip()
+        # Get the naming prefix, subfolder name, and image count
+        prefix = self.naming_prefix.get().strip()
+        subfolder = self.subfolder_name.get().strip()
+        count = self.image_count.get().strip()
 
-            if not count.isdigit():
-                self.show_popup("Invalid counter value. It must be an integer.")
-                self.button2.config(state="normal", text=original_text, bg=original_color)
-                return
+        # Check if the counter value is an integer
+        if not count.isdigit():
+            self.show_popup("Invalid counter value. It must be an integer.")
+            self.revert_button(self.button2, original_text, original_color)
+            return
 
-            self.count = int(count)
+        self.count = int(count)
 
-            subfolder_path = os.path.join(self.save_directory_path, subfolder)
-            os.makedirs(subfolder_path, exist_ok=True)
+        # Create the subfolder if it does not exist
+        subfolder_path = os.path.join(self.save_directory_path, subfolder)
+        os.makedirs(subfolder_path, exist_ok=True)
 
-            if prefix:
-                filename = f"{prefix}_image{self.count}.jpg"
-            else:
-                filename = f"image_{self.count}_Exposure{int(self.Set_exposure)}.jpg"
+        # Save the image with the appropriate naming pattern
+        if prefix:
+            filename = f"{prefix}_image{self.count}.jpg"
+        else:
+            filename = f"image_{self.count}_Exposure{int(self.Set_exposure)}.jpg"
 
-            file_path = os.path.join(subfolder_path, filename)
-            cv2.imwrite(file_path, self.image_buffer)
-            print(f"Saved image {self.count} as {filename}")
-            self.count += 1
+        # Save the image and update the image count
+        file_path = os.path.join(subfolder_path, filename)
+        cv2.imwrite(file_path, self.image_buffer)
+        print(f"Saved image {self.count} as {filename}")
+        self.count += 1
 
-            self.image_count_label.config(text=f"Images Saved: {self.count - 1}")
-            self.image_count.set(str(self.count))
-            self.image_buffer = None
-            self.show_popup(f"Image saved as {filename}")
+        # Update the image count label and reset the image buffer
+        self.image_count_label.config(text=f"Images Saved: {self.count - 1}")
+        self.image_count.set(str(self.count))
+        self.image_buffer = None
+        self.show_popup(f"Image saved as {filename}")
 
-            def revert_button():
-                self.button2.config(state="normal", text=original_text, bg=original_color)
-                self.root.update_idletasks()
+        # Revert the button style after 0.2seconds
+        self.root.after(2000, lambda: self.revert_button(self.button3, original_text, original_color))
 
-            self.root.after(1000, revert_button)
-
+    # Show a popup message for a short duration
     def show_popup(self, message):
         popup = tk.Toplevel(self.root)
         popup.title("Notification")
@@ -264,13 +285,23 @@ class ImageSaverApp:
         label.pack()
         popup.after(1000, popup.destroy)
 
+    # Open the save directory in the file explorer
     def open_explorer(self):
         if platform == "win32":
-            subprocess.run(['explorer', os.path.abspath(self.save_directory_path)])
+            subprocess.run(["explorer", os.path.abspath(self.save_directory_path)])
         elif platform == "darwin":
-            subprocess.run(['open', os.path.abspath(self.save_directory_path)])
-        elif platform.startswith('linux'):
-            subprocess.run(['xdg-open', os.path.abspath(self.save_directory_path)])
+            subprocess.run(["open", os.path.abspath(self.save_directory_path)])
+        elif platform.startswith("linux"):
+            subprocess.run(["xdg-open", os.path.abspath(self.save_directory_path)])
+
+
+# Function to properly close the camera when the application is closed
+def on_closing():
+    print("Closing application...")
+    for frame in app.frame_list:
+        Camera_off(frame)
+    app.root.destroy()
+
 
 # Example usage
 if __name__ == "__main__":
@@ -279,4 +310,5 @@ if __name__ == "__main__":
     save_directory_path = "images/"
     Set_exposure = 2000.0
     app = ImageSaverApp(root, save_directory_path, Set_exposure)
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
--- camera_setup.py
+++ camera_setup.py
@@ -12,9 +12,9 @@ from arena_api.__future__.save import Writer
 from multiprocessing import Value
 import json
 
-
 width1 = 2048
 height1 = 1536
+
 
 def set_node_value(nodemap, node, value):
     node_obj = nodemap.get_node(node)
@@ -22,6 +22,7 @@ def set_node_value(nodemap, node, value):
         node_obj.value = value
     else:
         print(f"Node {node} not found")
+
 
 def get_node_value(nodemap, node):
     node_obj = nodemap.get_node(node)
@@ -31,52 +32,47 @@ def get_node_value(nodemap, node):
         print(f"Node {node} not found")
         return None
 
+
 def create_devices_with_tries():
-            with threading.Lock():
-                tries = 0
-                tries_max = 6
-                sleep_time_secs = 10
-                while tries < tries_max: 
-                    devices = system.create_device()
-                    if not devices:
-                        safe_print(
-                            f'Try {tries+1} of {tries_max}: waiting for {sleep_time_secs} '
-                            f'secs for a device to be connected!')
-                        for sec_count in range(sleep_time_secs):
-                            time.sleep(1)
-                            safe_print(f'{sec_count + 1 } seconds passed ',
-                                '.' * sec_count, end='\r')
-                        tries += 1
-                    else:
-                        safe_print(f'Created {len(devices)} device(s)')
-                        return devices
-                else:
-                    raise Exception(f'No device found! Please connect a device and run '
-                                    f'the example again.')
+    with threading.Lock():
+        tries = 0
+        tries_max = 6
+        sleep_time_secs = 10
+        while tries < tries_max:
+            devices = system.create_device()
+            if not devices:
+                safe_print(
+                    f"Try {tries+1} of {tries_max}: waiting for {sleep_time_secs} "
+                    f"secs for a device to be connected!"
+                )
+                for sec_count in range(sleep_time_secs):
+                    time.sleep(1)
+                    safe_print(f"{sec_count + 1 } seconds passed ", "." * sec_count, end="\r")
+                tries += 1
+            else:
+                safe_print(f"Created {len(devices)} device(s)")
+                return devices
+        else:
+            raise Exception(f"No device found! Please connect a device and run " f"the example again.")
 
 
 def Camera_On(Set_exposure, which_camera, device, threading_event, threading_condition):
-
     class Video_Capture:
-
         def __init__(self, Set_exposure, which_camera, device, threading_event, threading_condition):
             self.frame_holder = None
-
             self.device = device
             self.which_camera = which_camera
             self.threading_event = threading_event
             self.threading_condition = threading_condition
             self.working_properly = False
             self.num_channels = 3
-
             self.setup(Set_exposure)
 
-            
+        # Start stream in a separate thread
         def startProcess(self):
-            t = threading.Thread(target=self.start_stream, args = ())
+            t = threading.Thread(target=self.start_stream, args=())
             t.daemon = True
             t.start()
-
 
         def setup(self, Set_exposure):
             nodemap = self.device.nodemap
@@ -105,48 +101,56 @@ def Camera_On(Set_exposure, which_camera, device, threading_event, threading_con
             set_node_value(device.nodemap, "GevSCPD", packet_delay)
             set_node_value(device.nodemap, "GevSCFTD", transmission_delay)
 
-            # Frame rate
+            # Eanble Max Frame rate
             acquisition_frame_rate = device.nodemap.get_node("AcquisitionFrameRate")
             acquisition_frame_rate.value = acquisition_frame_rate.max
             print(f"acquisition_frame_rate: {get_node_value(device.nodemap, 'AcquisitionFrameRate')}")
 
-            # # PTPSyncFrameRate
+            # PTPSyncFrameRate (for some cameras, this node may not be available)
             # print(f"{TAB3}ptp_sync_frame_rate: {get_node_value(device.nodemap, 'PTPSyncFrameRate')}")
             # ptp_sync_frame_rate = device.nodemap.get_node("PTPSyncFrameRate")
             # ptp_sync_frame_rate.value = 5.0
-            # print(f"{TAB3}ptp_sync_frame_rate: {get_node_value(device.nodemap, 'PTPSyncFrameRate')}")
-            
-            return self.num_channels
 
+            return self.num_channels
 
         def start_stream(self):
             safe_print(f"Camera_{self.which_camera} starts streaming.")
 
             with self.device.start_stream():
                 self.working_properly = True
+
+                # Continuously get buffer
                 while True:
-                    time.sleep(0.3)
                     with threading.Lock():
                         safe_print(self.threading_event)
                         buffer = self.device.get_buffer()
+
+                        # Convert buffer data to a numpy array
                         item = BufferFactory.copy(buffer)
+                        buffer_bytes_per_pixel = int(len(item.data) / (item.width * item.height))
+                        array = (ctypes.c_ubyte * self.num_channels * item.width * item.height).from_address(
+                            ctypes.addressof(item.pbytes)
+                        )
+                        npndarray = np.ndarray(
+                            buffer=array, dtype=np.uint8, shape=(item.height, item.width, buffer_bytes_per_pixel)
+                        )
 
-                        buffer_bytes_per_pixel = int(len(item.data)/(item.width * item.height))
-
-                        array = (ctypes.c_ubyte * self.num_channels * item.width * item.height).from_address(ctypes.addressof(item.pbytes))
-                    
-                        npndarray = np.ndarray(buffer=array, dtype=np.uint8, shape=(item.height, item.width, buffer_bytes_per_pixel))
-                        
+                        # Save the buffer to the holder
                         self.frame_holder = (npndarray, self.which_camera)
+
+                        # Notify the main app that this camera has finished pushing buffer
                         self.threading_event.set()
 
+                    # Wait for other cameras to finish pushing buffer
                     with self.threading_condition:
                         safe_print(f"Camera_{self.which_camera} finished pushing buffer. Waiting for other Cameras.")
+
+                        # Waiting for main app to receive all buffers and successfully display them
                         self.threading_condition.wait()
 
+                    # Reset the memory usgae of the buffer
                     BufferFactory.destroy(item)
                     self.device.requeue_buffer(buffer)
-
 
         def read(self):
             return self.frame_holder
@@ -154,15 +158,14 @@ def Camera_On(Set_exposure, which_camera, device, threading_event, threading_con
         def stop_stream(self):
             if self.working_properly:
                 self.device.stop_stream()
-                self.device.nodemap['UserSetSelector'].value = 'Default'
-                self.device.nodemap['UserSetLoad'].execute()
+                self.device.nodemap["UserSetSelector"].value = "Default"
+                self.device.nodemap["UserSetLoad"].execute()
 
-            print(f"Shutting camera_{self.which_camera}")
-
-
+                print(f"Shutting camera_{self.which_camera}")
 
     frame0 = Video_Capture(Set_exposure, which_camera, device, threading_event, threading_condition)
     return frame0
+
 
 def Camera_off(frame):
     frame.stop_stream()
@@ -171,4 +174,3 @@ def Camera_off(frame):
 def safe_print(*args, **kwargs):
     with threading.Lock():
         print(*args, **kwargs)
-
